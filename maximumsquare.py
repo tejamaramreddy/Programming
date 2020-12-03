1e4462cd46daf145c74469f8095f3ce03e83b891
Ravi Teja <tejamaramreddy@gmail.com>
2020-12-03 07:51:16 +0530

    Finding largest square matrix with 1's in a given binary matrix

---
+++ maximumsquare.py
@@ -0,0 +1,24 @@
+matrix =  [ [ 1, 1, 0, 0, 1, 1 ],
+            [ 0, 0, 1, 0, 1, 1 ],
+            [ 1, 1, 1, 1, 1, 0 ],
+            [ 1, 1, 1, 1, 1, 1 ],
+            [ 1, 1, 1, 1, 1, 1 ],
+            [ 0, 1, 1, 1, 1, 1 ],
+            [ 1, 0, 0, 0, 1, 1 ]]
+table = [[0]*len(matrix[0])]*len(matrix)
+r = len(matrix)
+c = len(matrix[0])
+max_size = 0
+for i in range(r):
+    for j in range(c):
+        if(i == 0 or j == 0):
+            print(i,j)
+            table[i][j] = matrix[i][j]
+            max_size = table[i][j] if table[i][j] > max_size else max_size
+        elif(matrix[i][j] == 0):
+            table[i][j] = 0
+        else:
+            table[i][j] = min(table[i-1][j],table[i][j-1],table[i-1][j-1]) + 1
+            max_size = table[i][j] if table[i][j] > max_size else max_size
+print(table)
+print(max_size)

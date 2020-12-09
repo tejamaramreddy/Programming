0d7a57597d40ea28c3c2b0fc145ad01fe9840a98
Ravi Teja <tejamaramreddy@gmail.com>
2020-12-09 10:23:57 +0530

    finding all duplicate elements in a array using Dict with O(n) time and space complexity

--- duplicatesinorder.py
+++ duplicatesinorder.py
@@ -1,10 +1,24 @@
-arr = [1, 2, 3, 1, 3, 6, 6]
+arr = [0, 3, 1, 3, 0]
 found  = False
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        if arr[i] == arr[j]:
-            found = True
-            print(arr[i])
-            continue
+# for i in range(len(arr)):
+#     for j in range(i+1,len(arr)):
+#         if arr[i] == arr[j]:
+#             found = True
+#             print(arr[i])
+#             continue
+# if found == False:
+#     print(-1)
+
+map = {}
+for i in arr:
+    if i in map:
+        map[i] += 1
+    else:
+        map[i] = 1
+
+for i in map:
+    if map[i] > 1:
+        found = True
+        print(i)
 if found == False:
     print(-1)

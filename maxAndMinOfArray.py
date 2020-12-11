6591139123fe796cc85a6131e3060d7328acb8a0
Ravi Teja <tejamaramreddy@gmail.com>
2020-12-11 08:56:07 +0530

    finding maximum and minimum elements of an array

---
+++ maxAndMinOfArray.py
@@ -0,0 +1,14 @@
+def maxandmin(arr):
+    max = arr[0]
+    min = arr[0]
+    for i in arr:
+        if i > max:
+            max = i
+        if i < min:
+            min = i
+    print(min,max)
+
+arr = [3, 2, 1, 56, 10000, 167]
+maxandmin(arr)
+arr = [1, 345, 234, 21, 56789]
+maxandmin(arr)
